095cafef0f2d6a9bfe6fbc538c5d8e0a5ed311d2
benvilliere <benoit.villiere@icloud.com>
2024-02-29 18:11:25 +0000

    Auto-commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+// CLI entry point
+const process = require("process");
+const args = process.argv.slice(2); // Get CLI arguments
+
+// Require your watch script
+const watch = require("./watch");
+
+// Check the command provided
+if (args.length > 0 && args[0] === "start") {
+  console.log("Starting artelier...");
+  watch(); // This will start the file watching
+} else {
+  console.log("Usage: artelier start");
+}
--- src/watch.js
+++ src/watch.js
@@ -1,21 +1,26 @@
 const chokidar = require("chokidar");
 const simpleGit = require("simple-git");
 
-// Set up file watcher
-const watcher = chokidar.watch(".", {
-  ignored: /(^|[\/\\])\../,
-});
+const watch = () => {
+  // Set up file watcher
+  const watcher = chokidar.watch(".", {
+    ignored: /(^|[\/\\])\../,
+  });
 
-// Function to handle the 'change' event
-const onChange = (path) => {
-  console.log(`File ${path} has been changed`);
-  const git = simpleGit();
-  git
-    .add(".")
-    .then(() => git.commit("Auto-commit"))
-    .then(() => console.log("Changes committed"))
-    .catch((err) => console.error("Failed to commit changes:", err));
+  // Function to handle the 'change' event
+  const onChange = (path) => {
+    console.log(`File ${path} has been changed`);
+    const git = simpleGit();
+    git
+      .add(".")
+      .then(() => git.commit("Auto-commit"))
+      .then(() => console.log("Changes committed"))
+      .catch((err) => console.error("Failed to commit changes:", err));
+  };
+
+  // Add event listener for file changes
+  watcher.on("change", onChange);
 };
 
-// Add event listener for file changes
-watcher.on("change", onChange);
+// Export the function
+module.exports = watch;
