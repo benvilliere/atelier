5f0ddd71fbd11b64d72355e250c4ef29d20a2cca
benvilliere <22590566+benvilliere@users.noreply.github.com>
2024-02-29 21:24:16 +0000

    Auto-commit

--- src/watch.js
+++ src/watch.js
@@ -12,8 +12,10 @@ const watch = async () => {
   const server = await createServer(config.vite); // Pass Vite-specific configurations from loaded config
 
   await server.listen();
-  console.log(server);
-  // console.log(`Atelier running at ${url}`);
+  const url = `http://${config.server?.host || "localhost"}:${
+    config.server?.port || 3000
+  }`;
+  console.log(`Atelier running at ${url}`);
 
   const git = simpleGit();
 
