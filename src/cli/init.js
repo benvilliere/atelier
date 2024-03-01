f375aac61f63ab55b94ad17bf947cacfc78ef2ae
benvilliere <benoit.villiere@icloud.com>
2024-03-01 18:50:00 +0000

    Atelier auto-commit

--- src/cli/init.js
+++ src/cli/init.js
@@ -1,5 +1,9 @@
 import path from "path";
-import { ATELIER_CONFIG_FILE_NAME, ATELIER_DOT_DIR } from "../constants.js";
+import {
+  ATELIER_CONFIG_FILE_NAME,
+  ATELIER_DOT_DIR,
+  ATELIER_CAPTURE_DIR_NAME,
+} from "../constants.js";
 import { createConfigFile } from "../config.js";
 import { createDirectory } from "../helpers.js";
 
@@ -7,7 +11,10 @@ export default async function init() {
   const projectRoot = process.cwd();
   const atelierConfigPath = path.join(projectRoot, ATELIER_CONFIG_FILE_NAME);
   const atelierDirPath = path.join(projectRoot, ATELIER_DOT_DIR);
-  const screenshotsDirPath = path.join(atelierDirPath, "screenshots");
+  const screenshotsDirPath = path.join(
+    atelierDirPath,
+    ATELIER_CAPTURE_DIR_NAME
+  );
 
   await createDirectory(atelierDirPath);
   await createDirectory(screenshotsDirPath);
--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,3 @@
 export const ATELIER_DOT_DIR = ".atelier";
 export const ATELIER_CONFIG_FILE_NAME = "atelier.json";
+export const ATELIER_CAPTURE_DIR_NAME = "capture";
