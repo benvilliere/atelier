b0776b506c986ea9b03e7710dfd928e5f8f252b1
benvilliere <benoit.villiere@icloud.com>
2024-03-01 00:50:40 +0000

    Atelier auto-commit

--- src/cli/index.js
+++ src/cli/index.js
@@ -8,8 +8,7 @@ const args = process.argv.slice(2);
 
 if (args.length > 0 && args[0] === "start") {
   const command = args[0];
-  switch command {
-    
+  switch (command) {
   }
   console.log("Starting atelier...");
   watch();
