d7d80f399fc6f25e1e889f3fa5a7ffbb6dbb0bee
benvilliere <benoit.villiere@icloud.com>
2024-03-01 23:23:08 +0000

    Atelier auto-commit

--- src/config/base.js
+++ src/config/base.js
@@ -13,7 +13,7 @@ export default {
     start: "npm run dev",
   },
   watch: {
-    include: ["**/*.js"],
+    include: ["**/*.js", "**/*.css", "**/*.html"],
     exclude: [".atelier/", "node_modules/", "dist/", "build/"],
   },
   server: {
