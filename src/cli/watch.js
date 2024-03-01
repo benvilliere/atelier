142e9a8ae5592fa51b04adb8b6716a50e99a9197
benvilliere <22590566+benvilliere@users.noreply.github.com>
2024-03-01 00:22:02 +0000

    Atelier auto-commit

--- src/cli/watch.js
+++ src/cli/watch.js
@@ -20,7 +20,7 @@ const watch = async () => {
     server: config.server,
   });
 
-  await server.listen(config.server?.port || 3000);
+  await server.listen(config.server?.port || 4242);
 
   // Get the local server URL
   const url = server.resolvedUrls.local[0];
