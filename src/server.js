fccc97b98e7a045f79acb25515e4f207a623d767
benvilliere <benoit.villiere@icloud.com>
2024-03-01 18:35:50 +0000

    Atelier auto-commit

---
+++ src/server.js
@@ -0,0 +1,8 @@
+export async function initializeServer(config) {
+  const server = await createServer({
+    root: config.root || ".",
+    server: config.server,
+  });
+  await server.listen(config.server.port || 4242);
+  return server;
+}
