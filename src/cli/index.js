6d4b650de8356d7d0bfab02c47738d30976b5986
benvilliere <benoit.villiere@icloud.com>
2024-03-01 00:51:48 +0000

    Atelier auto-commit

--- src/cli/index.js
+++ src/cli/index.js
@@ -5,19 +5,14 @@ import init from "./init.js";
 import watch from "./watch.js";
 
 const args = process.argv.slice(2);
+const command = args[0];
 
-if (args.length > 0) {
-  const command = args[0];
-
-  switch (command) {
-    case "start":
-      console.log("Starting atelier...");
-      watch();
-    case "init":
-      init();
-    default:
-      console.log("Usage: atelier start");
-  }
-} else {
-  console.log("Usage: atelier start");
+switch (command) {
+  case "start":
+    console.log("Starting atelier...");
+    watch();
+  case "init":
+    init();
+  default:
+    console.log("Usage: atelier start");
 }
