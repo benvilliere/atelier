314d7c10cc80ff424672ef57573813cde0c933fd
benvilliere <benoit.villiere@icloud.com>
2024-03-01 18:34:30 +0000

    Atelier auto-commit

---
+++ src/git.js
@@ -0,0 +1,8 @@
+async function commitChanges(config) {
+  const git = simpleGit();
+  await git.add(".");
+  const { commit: hash } = await git.commit(
+    config.git?.commit?.message || "Atelier auto-commit"
+  );
+  return hash;
+}
