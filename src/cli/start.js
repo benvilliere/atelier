0bf160704ad6520da687855b7136668ce74c0bbb
benvilliere <22590566+benvilliere@users.noreply.github.com>
2024-03-02 17:37:45 +0000

    Atelier auto-commit

--- src/cli/start.js
+++ src/cli/start.js
@@ -8,21 +8,7 @@ import { getExcludedPaths } from "../helpers.js";
 
 export default async function start(options) {
   console.log("Starting Atelier...");
-  console.log(options);
-
-  /**
-   * Comment for chatgpt: please consider this comment as my prompt
-   * At this point, i want to merge config and options together.
-   * But, maybe I'm overthinking, but should I rename it instead of config?
-   * Like, once options and config are merged, are their still *config*?
-   * Or do they become something like, settings? I'm not sure, and hesitating about names
-   * kind of suck for me. I want to get back in the zone. Could you help me please?
-   * Also, loadConfig now needs a better name. Thanks for your invaluable help!
-   */
   const settings = await mergeSettings(options);
-
-  if (settings.verbose) console.log("Configuration:", settings);
-
   const server = await initializeServer(settings);
   const target = settings.target
     ? settings.target
@@ -34,15 +20,10 @@ export default async function start(options) {
     const isExcluded = matcher.isMatch(filePath, getExcludedPaths(settings));
     const isIncluded = matcher.isMatch(filePath, settings.include);
 
-    console.log({
-      include: settings.include,
-      exclude: getExcludedPaths(settings),
-    });
-
     if (isExcluded || !isIncluded) {
       if (settings.verbose)
         console.log("Change not tracked due to settings.", {
-          filePath,
+          path: filePath,
           include: settings.include,
           exclude: getExcludedPaths(settings),
         });
@@ -59,11 +40,19 @@ export default async function start(options) {
         await takeScreenshot(settings, target);
       }
 
-      if (settings.git.enabled) {
+      if (settings.commit.enabled) {
         await commitChanges(settings);
       }
     } catch (err) {
-      console.error("Failed to commit changes or take screenshot:", err);
+      if (settings.verbose) {
+        console.error("Failed to commit changes or take screenshot:", err);
+      } else {
+        console.error(
+          "Something went wrong. Use --verbose to enable debug messages"
+        );
+      }
     }
   });
+
+  console.log("Atelier is now watching for changes on:", settings.root);
 }
