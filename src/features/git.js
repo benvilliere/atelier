bfe756b8bc28cb24faf408ce2aa93e211b4f550a
benvilliere <benoit.villiere@icloud.com>
2024-03-03 22:20:33 +0000

    🎨 @genart/atelier: auto-commit on save

--- src/features/git.js
+++ src/features/git.js
@@ -6,6 +6,6 @@ export async function commitChanges(settings) {
   const { commit: hash } = await git.commit(
     settings.commit.message || "Atelier auto-commit"
   );
-  if (settings.verbose) console.log("Changes committed", hash);
+  if (settings.verbose) console.log("Changes committed", { hash });
   return hash;
 }
