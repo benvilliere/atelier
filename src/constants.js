99309ad33c8098b5521ab656149ce6e04f06e8ef
benvilliere <benoit.villiere@icloud.com>
2024-03-03 22:11:19 +0000

    🎨 @genart/atelier: auto-commit on save

--- src/constants.js
+++ src/constants.js
@@ -2,3 +2,4 @@ export const ATELIER_BASE_DIR = ".atelier";
 export const ATELIER_CONFIG_FILE_NAME = "atelier.json";
 export const ATELIER_RECORDING_DIR = "recording";
 export const ATELIER_SCREENSHOT_DIR = "screenshot";
+export const ATELIER_TIMELINE_DIR = "timeline";
