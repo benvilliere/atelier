5495152ce25e7c33c9c5b1d15ae938af185532c5
benvilliere <22590566+benvilliere@users.noreply.github.com>
2024-03-03 22:21:44 +0000

    🎨 @genart/atelier: auto-commit on save

--- src/features/git.js
+++ src/features/git.js
@@ -3,9 +3,7 @@ import simpleGit from "simple-git";
 export async function commitChanges(settings) {
   const git = simpleGit();
   await git.add(".");
-  const { commit: hash } = await git.commit(
-    settings.commit.message || "Atelier auto-commit"
-  );
+  const { commit: hash } = await git.commit(settings.commit.message);
   if (settings.verbose) console.log("Changes committed", { hash });
   return hash;
 }
