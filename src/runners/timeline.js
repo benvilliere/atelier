2ef6ebd0c81a4c9e355902bf74ad5e2b65f8466c
benvilliere <22590566+benvilliere@users.noreply.github.com>
2024-03-03 22:45:43 +0000

    🎨 @genart/atelier: auto-commit on save

--- src/runners/timeline.js
+++ src/runners/timeline.js
@@ -7,7 +7,7 @@ export default async function timeline(settings) {
     const url = `http://localhost:${port}`;
 
     backend.listen(port, () => {
-      console.log(`Timeline running at ${url}`);
+      console.log(`💫 Timeline running at ${url}`);
 
       if (settings.timeline.open) {
         openBrowser(url);
