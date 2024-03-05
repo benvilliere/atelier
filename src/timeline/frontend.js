3f9f9621b7025c29aec199713b104845f7b42e30
benvilliere <22590566+benvilliere@users.noreply.github.com>
2024-03-05 21:26:11 +0000

    🎨 @genart/atelier: auto-commit on save

--- src/timeline/frontend.js
+++ src/timeline/frontend.js
@@ -14,7 +14,7 @@ function timeAgo(timestamp) {
     { seconds: 60, text: "minute" },
   ];
 
-  const now = new Date() * 1000;
+  const now = new Date(Date.now() * 1000);
   const then = new Date(timestamp * 1000);
   const elapsed = then - now;
   return elapsed;
