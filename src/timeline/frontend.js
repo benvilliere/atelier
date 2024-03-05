7799fc05bd10e5cb7ab13f81af024272672b23c3
benvilliere <benoit.villiere@icloud.com>
2024-03-05 21:26:02 +0000

    🎨 @genart/atelier: auto-commit on save

--- src/timeline/frontend.js
+++ src/timeline/frontend.js
@@ -15,7 +15,7 @@ function timeAgo(timestamp) {
   ];
 
   const now = new Date() * 1000;
-  const then = new Date(timestamp) * 1000;
+  const then = new Date(timestamp * 1000);
   const elapsed = then - now;
   return elapsed;
   if (elapsed < 60) {
