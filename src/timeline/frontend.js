43467192f3a4b6869c930d4db41a0bbbee3f0e0e
benvilliere <benoit.villiere@icloud.com>
2024-03-07 22:50:17 +0000

    🎨 @genart/atelier: auto-commit on save

--- src/timeline/frontend.js
+++ src/timeline/frontend.js
@@ -101,35 +101,32 @@ document.addEventListener("alpine:init", () => {
       window.addEventListener(
         "scroll",
         async () => {
-          await onScrollEnd();
+          if (this.fetchingMoreEntries) {
+            console.log("Already fetching more entries");
+            return;
+          }
+          if (
+            window.innerHeight + window.scrollY >=
+            document.documentElement.scrollHeight - 300
+          ) {
+            if (this.timeline.page < this.timeline.totalPages) {
+              console.log("you're at the bottom of the page");
+
+              this.fetchingMoreEntries = true;
+
+              const data = await getTimeline(this.timeline.page + 1);
+
+              this.timeline = {
+                ...data,
+                entries: [...this.timeline.entries, ...data.entries],
+              };
+
+              this.fetchingMoreEntries = false;
+            }
+          }
         },
         { passive: true }
       );
-    },
-    async onScrollEnd() {
-      if (this.fetchingMoreEntries) {
-        console.log("Already fetching more entries");
-        return;
-      }
-      if (
-        window.innerHeight + window.scrollY >=
-        document.documentElement.scrollHeight - 300
-      ) {
-        if (this.timeline.page < this.timeline.totalPages) {
-          console.log("you're at the bottom of the page");
-
-          this.fetchingMoreEntries = true;
-
-          const data = await getTimeline(this.timeline.page + 1);
-
-          this.timeline = {
-            ...data,
-            entries: [...this.timeline.entries, ...data.entries],
-          };
-
-          this.fetchingMoreEntries = false;
-        }
-      }
     },
   });
 });
