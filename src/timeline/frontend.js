c87e8345c6a260fcd6d55156757acfd9c6dbfda5
benvilliere <22590566+benvilliere@users.noreply.github.com>
2024-03-07 17:44:56 +0000

    🎨 @genart/atelier: auto-commit on save

--- src/timeline/frontend.js
+++ src/timeline/frontend.js
@@ -59,3 +59,13 @@ async function copyImageToClipboard(imgId) {
     }
   }, "image/png");
 }
+
+document.addEventListener("alpine:init", () => {
+  Alpine.store("darkMode", {
+    on: false,
+
+    toggle() {
+      this.on = !this.on;
+    },
+  });
+});
