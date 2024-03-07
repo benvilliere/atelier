a25ce8ad0488ff1d9808e551842063634448f75d
benvilliere <benoit.villiere@icloud.com>
2024-03-07 21:52:04 +0000

    🎨 @genart/atelier: auto-commit on save

--- src/features/database.js
+++ src/features/database.js
@@ -11,25 +11,6 @@ export function newDataEntry() {
   };
 }
 
-export async function saveEntry(data, settings) {
-  data.timestamp = Date.now();
-
-  const filename = `${data.timestamp}.json`;
-  const filePath = path.join(getTimelineDir(settings), filename);
-
-  await mkdir(getTimelineDir(settings), { recursive: true });
-  await writeFile(filePath, JSON.stringify(data, null, 2), "utf8");
-
-  if (settings.verbose) {
-    console.log("💾 Data saved", {
-      filePath,
-      data,
-    });
-  }
-
-  return filePath;
-}
-
 export async function saveData(data, settings) {
   const entry = newDataEntry();
 
@@ -54,4 +35,23 @@ export async function saveData(data, settings) {
 
     await saveEntry(entry, settings);
   }
+}
+
+export async function saveEntry(data, settings) {
+  data.timestamp = Date.now();
+
+  const filename = `${data.timestamp}.json`;
+  const filePath = path.join(getTimelineDir(settings), filename);
+
+  await mkdir(getTimelineDir(settings), { recursive: true });
+  await writeFile(filePath, JSON.stringify(data, null, 2), "utf8");
+
+  if (settings.verbose) {
+    console.log("💾 Data saved", {
+      filePath,
+      data,
+    });
+  }
+
+  return filePath;
 }
