9a111acca0e078277dcaa275f5913520b881fce7
benvilliere <benoit.villiere@icloud.com>
2024-03-08 17:58:32 +0000

    🎨 @genart/atelier: auto-commit on save

--- src/features/screenshot.js
+++ src/features/screenshot.js
@@ -1,5 +1,7 @@
 import { mkdir } from "fs/promises";
 import puppeteer from "puppeteer";
+import { setTimeout } from "node:timers/promises";
+
 import { getScreenshotDir, sleep } from "../helpers.js";
 
 export async function takeScreenshot(settings) {
