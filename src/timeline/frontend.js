ca267ba5a9de20ecb80de3f39dd0b2e85d382262
benvilliere <benoit.villiere@icloud.com>
2024-03-08 00:05:26 +0000

    🎨 @genart/atelier: auto-commit on save

--- src/timeline/frontend.js
+++ src/timeline/frontend.js
@@ -103,6 +103,8 @@ document.addEventListener("alpine:init", () => {
           };
 
           this.newEntries += newEntriesAmount;
+
+          // Show only if not viewing the top of the page
           this.showNewEntriesPill =
             window.scrollY >
             document.getElementById("atelier-card-1").clientHeight;
