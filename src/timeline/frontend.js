ee25bec4bface027a310a9ee91d4ca57fffa2013
benvilliere <benoit.villiere@icloud.com>
2024-03-10 14:10:05 +0000

    🎨 @genart/atelier: auto-commit on save

--- src/timeline/frontend.js
+++ src/timeline/frontend.js
@@ -2,10 +2,81 @@ const { createBrowserRouter, RouterProvider, Route, Link } =
   window.ReactRouterDOM;
 const { useRoutes, useNavigate } = window.ReactRouter;
 
+async function get(endpoint) {
+  return await (await fetch(endpoint)).json();
+}
+
+function formatDate(timestamp) {
+  console.log(timestamp);
+  const date = new Date(timestamp * 1000);
+  return date.toLocaleString();
+}
+
+function timeAgo(timestamp) {
+  const intervals = [
+    { seconds: 31536000, text: "year" },
+    { seconds: 2592000, text: "month" },
+    { seconds: 604800, text: "week" },
+    { seconds: 86400, text: "day" },
+    { seconds: 3600, text: "hour" },
+    { seconds: 60, text: "minute" },
+  ];
+
+  const now = new Date();
+  const elapsed = Math.floor((now - new Date(timestamp)) / 1000);
+
+  if (elapsed < 60) {
+    return "New";
+  }
+
+  for (let { seconds, text } of intervals) {
+    const interval = Math.floor(elapsed / seconds);
+    if (interval >= 1) {
+      return `${interval} ${text}${interval > 1 ? "s" : ""} ago`;
+    }
+  }
+}
+
+async function copyImageToClipboard(imgId) {
+  const img = document.getElementById(imgId);
+
+  // Create an off-screen canvas
+  const canvas = document.createElement("canvas");
+  const ctx = canvas.getContext("2d");
+
+  // Set canvas dimensions to match the image
+  canvas.width = img.naturalWidth;
+  canvas.height = img.naturalHeight;
+
+  // Draw the image onto the canvas
+  ctx.drawImage(img, 0, 0);
+
+  // Convert the canvas to a Blob
+  canvas.toBlob(async (blob) => {
+    try {
+      // Create a new ClipboardItem object
+      const item = new ClipboardItem({ "image/png": blob });
+      // Copy the ClipboardItem to the clipboard
+      await navigator.clipboard.write([item]);
+    } catch (err) {
+      console.error("Failed to copy image: ", err);
+    }
+  }, "image/png");
+}
+
+async function getTimeline(page = 1, limit = 32) {
+  console.log("Fetching page:", page);
+  return await get(`/timeline?page=${page}&limit=${limit}`);
+}
+
+async function getTimelineSince(when) {
+  console.log("Fetching since:", when);
+  return await get(`/timeline/since/${when}`);
+}
+
 function Logo() {
   const handleLogoClick = (event) => {
     event.preventDefault();
-    console.log("test");
     window.scrollTo(0, 0);
   };
 
