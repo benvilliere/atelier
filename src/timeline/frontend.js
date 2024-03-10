4844626d154a8f6e068ced0cfe8cb798973edcda
benvilliere <22590566+benvilliere@users.noreply.github.com>
2024-03-10 14:23:43 +0000

    🎨 @genart/atelier: auto-commit on save

--- src/timeline/frontend.js
+++ src/timeline/frontend.js
@@ -190,6 +190,36 @@ function DownloadAction({ artwork }) {
   if (!artwork.screenshot || !artwork.recording) {
     return;
   }
+
+  return ((
+            <div class="action">
+              <a
+                href={
+                  artwork.screenshot
+                    ? `/screenshots/${artwork.screenshot}`
+                    : `/recordings/${artwork.recording}`
+                }
+                download
+                title={`Download ${
+                  artwork.screenshot ? artwork.screenshot : artwork.recording
+                }`}
+              >
+                <span>Download</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
+                  />
+                </svg>
+              </a>
+            </div>)
 }
 
 function Card({ artwork, index }) {
