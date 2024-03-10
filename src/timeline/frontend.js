4bce0a1baed9334733cf52ccb9a843c57376f502
benvilliere <22590566+benvilliere@users.noreply.github.com>
2024-03-10 12:57:55 +0000

    🎨 @genart/atelier: auto-commit on save

--- src/timeline/frontend.js
+++ src/timeline/frontend.js
@@ -2,7 +2,34 @@ const { createBrowserRouter, RouterProvider, Route, Link } =
   window.ReactRouterDOM;
 const { useRoutes, useNavigate } = window.ReactRouter;
 
-import Logo from "/components/Logo.js";
+function Logo() {
+  const handleLogoClick = (event) => {
+    event.preventDefault();
+    console.log("test");
+    window.scrollTo(0, 0);
+  };
+
+  return (
+    <h1 class="logo">
+      <a href="#" onClick={handleLogoClick}>
+        <span>Atelier</span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18"
+          />
+        </svg>
+      </a>
+    </h1>
+  );
+}
 
 function Header() {
   const navigate = useNavigate();
