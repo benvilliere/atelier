78b52fa4b8eac72b61bd6dad4c28c2c76b6bfc3f
benvilliere <benoit.villiere@icloud.com>
2024-03-10 21:41:38 +0000

    🎨 @genart/atelier: auto-commit on save

--- src/timeline/frontend/api.js
+++ src/timeline/frontend/api.js
@@ -8,7 +8,7 @@ async function getTimeline(page = 1, limit = 32) {
 async function getArtworks({ page = 1, limit = 32, since = 0 }) {
   page = page || 1;
   limit = limit || 32;
-  since ||= 0;
+  since = since || 0;
   console.log("Fetching page:", page);
   return await get(`/timeline?page=${page}&limit=${limit}&since=${since}`);
 }
