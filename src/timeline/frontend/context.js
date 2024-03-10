7322c2d62b4a8adc169c1ec3d84642afd44597b7
benvilliere <22590566+benvilliere@users.noreply.github.com>
2024-03-10 21:15:13 +0000

    🎨 @genart/atelier: auto-commit on save

--- src/timeline/frontend/context.js
+++ src/timeline/frontend/context.js
@@ -59,6 +59,7 @@ const AtelierProvider = ({ children }) => {
 
     const timer = setInterval(async () => {
       const newArtworks = await polling();
+      console.log({ newArtworks });
       setArtworks([...newArtworks, ...artworks]);
     }, 3000);
     return () => clearInterval(timer);
