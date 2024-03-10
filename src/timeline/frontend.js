4d772de4821f428e08440528fe3dec77b5eee668
benvilliere <benoit.villiere@icloud.com>
2024-03-10 18:42:24 +0000

    🎨 @genart/atelier: auto-commit on save

--- src/timeline/frontend.js
+++ src/timeline/frontend.js
@@ -4,22 +4,6 @@ const { createContext, useContext, useEffect, useState } = window.React;
 
 /// API calls
 
-async function getTimeline(page = 1, limit = 32) {
-  console.log("Fetching page:", page);
-  return await get(`/timeline?page=${page}&limit=${limit}`);
-}
-
-async function getTimelineSince(when) {
-  console.log("Fetching since:", when);
-  return await get(`/timeline/since/${when}`);
-}
-
-async function deleteArtwork(artwork) {
-  await fetch(`/delete/${artwork.timestamp}`, {
-    method: "POST",
-  });
-}
-
 /// React context
 
 /// React components
