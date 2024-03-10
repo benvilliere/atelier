445f6641c4310f4fc9ee9e7b73c689c57f95f0d0
benvilliere <22590566+benvilliere@users.noreply.github.com>
2024-03-10 12:57:06 +0000

    🎨 @genart/atelier: auto-commit on save

---
+++ src/timeline/components/Logo.jsx
@@ -0,0 +1,28 @@
+export default function Logo() {
+  const handleLogoClick = (event) => {
+    event.preventDefault();
+    console.log("test");
+    window.scrollTo(0, 0);
+  };
+
+  return (
+    <h1 class="logo">
+      <a href="#" onClick={handleLogoClick}>
+        <span>Atelier</span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18"
+          />
+        </svg>
+      </a>
+    </h1>
+  );
+}
