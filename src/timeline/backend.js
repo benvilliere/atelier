5d121f1b3094581f3a9b871a08ce5274024d3343
benvilliere <benoit.villiere@icloud.com>
2024-03-10 12:20:30 +0000

    🎨 @genart/atelier: auto-commit on save

--- src/timeline/backend.js
+++ src/timeline/backend.js
@@ -69,6 +69,67 @@ export function createBackend(settings) {
     }
   });
 
+  async function getTimeline(limit, page, offset, since) {
+    const files = await fs.readdir(directories.data);
+
+    let entries = await Promise.all(
+      files
+        .filter((file) => path.extname(file) === ".json")
+        .filter((file) => file.replace(".json", "") > since)
+        .map(async (file) => {
+          const filePath = path.join(directories.data, file);
+          const fileContents = await fs.readFile(filePath, "utf8");
+          return JSON.parse(fileContents);
+        })
+    );
+
+    entries.sort((a, b) => b.timestamp - a.timestamp);
+
+    const paginatedItems = entries.slice(offset, offset + limit);
+
+    return {
+      entries: paginatedItems,
+      page,
+      limit,
+      total: entries.length,
+      totalPages: Math.ceil(entries.length / limit),
+    };
+  }
+
+  backend.get("/timeline/html", async (req, res) => {
+    try {
+      const limit = parseInt(req.query.limit, 10) || 32;
+      const page = parseInt(req.query.page, 10) || 1;
+      const offset = (page - 1) * limit;
+      const files = await fs.readdir(directories.data);
+
+      let entries = await Promise.all(
+        files
+          .filter((file) => path.extname(file) === ".json")
+          .map(async (file) => {
+            const filePath = path.join(directories.data, file);
+            const fileContents = await fs.readFile(filePath, "utf8");
+            return JSON.parse(fileContents);
+          })
+      );
+
+      entries.sort((a, b) => b.timestamp - a.timestamp);
+
+      const paginatedItems = entries.slice(offset, offset + limit);
+
+      res.json({
+        entries: paginatedItems,
+        page,
+        limit,
+        total: entries.length,
+        totalPages: Math.ceil(entries.length / limit),
+      });
+    } catch (err) {
+      console.error("Failed to load data:", err);
+      res.status(500).send("Failed to load data");
+    }
+  });
+
   backend.get("/timeline", async (req, res) => {
     try {
       const limit = parseInt(req.query.limit, 10) || 32;
