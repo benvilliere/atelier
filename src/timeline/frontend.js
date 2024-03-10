13da9e21f377c36e36bd086a209a0e457389b72c
benvilliere <benoit.villiere@icloud.com>
2024-03-10 21:51:02 +0000

    🎨 @genart/atelier: auto-commit on save

--- src/timeline/frontend.js
+++ src/timeline/frontend.js
@@ -28,52 +28,10 @@ function Logo() {
   );
 }
 
-function Pill() {
-  return (
-    <div class="alert" x-show="$store.atelier.showNewArtworksPill">
-      <a
-        class="pill"
-        href="/"
-        // @click.prevent="
-        //   window.scrollTo(0, 0);
-        //   $store.atelier.showNewArtworksPill = false;
-        // "
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
-          />
-        </svg>
-        <span
-          class="label"
-          x-show="$store.atelier.newArtworks > 1"
-          x-text="`${$store.atelier.newArtworks} new artworks`"
-        ></span>
-        <span
-          class="label"
-          x-show="$store.atelier.newArtworks === 1"
-          x-text="`${$store.atelier.newArtworks} new artwork`"
-        >
-          x new artworks
-        </span>
-      </a>
-    </div>
-  );
-}
-
 function Header() {
   return (
     <div class="header">
       <Logo />
-      <Pill />
     </div>
   );
 }
