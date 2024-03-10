1cab98a290b213559c648f7b5b9c7ce4df73f648
benvilliere <benoit.villiere@icloud.com>
2024-03-10 12:53:03 +0000

    🎨 @genart/atelier: auto-commit on save

--- src/timeline/frontend.js
+++ src/timeline/frontend.js
@@ -4,6 +4,30 @@ const { useRoutes, useNavigate } = window.ReactRouter;
 
 // Header component
 
+function Logo() {
+  return (
+    <h1 class="logo">
+        <a href="#" @click.prevent="window.scrollTo(0, 0);">
+          <span>Atelier</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18"
+            />
+          </svg>
+        </a>
+      </h1>
+  )
+  
+}
+
 function Header() {
   const navigate = useNavigate();
 
